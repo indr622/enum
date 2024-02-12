47f8eed13b2b73dbe55954ec0c955cb10626ad8b
indra <47154580+indr622@users.noreply.github.com>
2024-02-13 00:50:43 +0700

    chore : Add Command Make enum

---
+++ src/Commands/MakeEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace IndraBasuki\Enum\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeEnum extends Command
+{
+    protected $name = 'make:enum';
+
+    protected $description = 'Create a new enum class';
+
+    protected $type = 'Enum';
+
+    public function handle()
+    {
+        //
+    }
+}
